1347e6b1eb8347f8cf42bac594404bd6b5e8be33
ghost <10137+ghost@users.noreply.github.com>
2022-04-02 11:17:00 +0800

    fix(kernel,header): add LA kernel headers

---
+++ kernel/arch/loongarch64/include/arch/loongarch64/mmu.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <arch/defines.h>
+#include <sys/types.h>
+
+#define MMU_KERNEL_PAGE_SIZE_SHIFT      (PAGE_SIZE_SHIFT)
+
+#define MMU_LX_X(page_shift, level) ((4 - (level)) * ((page_shift) - 3) + 3)
+
+#define MMU_GUEST_SIZE_SHIFT    48
+
+// TODO: PTE size
+typedef uint64_t pte_t;
