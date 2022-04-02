33181ca5f4b44fd155f11e5c7f0233dd1fbe3796
ghost <10137+ghost@users.noreply.github.com>
2022-04-02 15:56:29 +0800

    fix(kernel,platform): add platform_dgetc boilerplate

--- kernel/platform/generic-loongarch/platform.cc
+++ kernel/platform/generic-loongarch/platform.cc
@@ -11,6 +11,16 @@ void platform_dputs_thread(const char* str, size_t len) {
 //     uart_puts(str, len, true, true);
 }
 
+int platform_dgetc(char* c, bool wait) {
+//  if (uart_disabled) {
+//    return ZX_ERR_NOT_SUPPORTED;
+//  }
+//  int ret = uart_getc(wait);
+//  if (ret < 0)
+//    return ret;
+//  *c = static_cast<char>(ret);
+  return 0;
+}
 size_t platform_recover_crashlog(size_t len, void* cookie,
                                  void (*func)(const void* data, size_t, size_t len, void* cookie)) {
 
