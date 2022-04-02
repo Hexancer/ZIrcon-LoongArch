30dcde78a88c748dcbafe4b659989d8d0c2385fc
ghost <10137+ghost@users.noreply.github.com>
2022-04-02 15:28:42 +0800

    fix(user,musl): weak alias for libcxx

--- third_party/ulib/musl/src/stdio/vsscanf.c
+++ third_party/ulib/musl/src/stdio/vsscanf.c
@@ -9,3 +9,5 @@ int vsscanf(const char* restrict s, const char* restrict fmt, va_list ap) {
     FILE f = {.buf = (void*)s, .cookie = (void*)s, .read = do_read, .lock = -1};
     return vfscanf(&f, fmt, ap);
 }
+
+weak_alias(vsscanf,__isoc99_vsscanf);
