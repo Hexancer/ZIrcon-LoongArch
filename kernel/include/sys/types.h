5d181703e80f56a67593b1e378850d165476d64a
ghost <10137+ghost@users.noreply.github.com>
2022-04-02 11:13:56 +0800

    fix(kernel,header): fix SSIZE_MAX redefinition

--- kernel/include/sys/types.h
+++ kernel/include/sys/types.h
@@ -25,7 +25,7 @@ typedef uintptr_t vaddr_t;
 typedef uintptr_t paddr_t;
 
 typedef intptr_t ssize_t;
-#define SSIZE_MAX INTPTR_MAX
+//#define SSIZE_MAX INTPTR_MAX
 
 #define KB (1024UL)
 #define MB (1024UL * KB)
