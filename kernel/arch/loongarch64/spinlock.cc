3d8caf7217bc36665d00a8415fd5a67623ef4fcd
ghost <10137+ghost@users.noreply.github.com>
2022-04-02 15:35:53 +0800

    fix(kernel,arch): add LA cacop mmu spinlock boilerplate

---
+++ kernel/arch/loongarch64/spinlock.cc
@@ -0,0 +1,7 @@
+#include <arch/ops.h>
+#include <arch/spinlock.h>
+#include <kernel/atomic.h>
+
+void arch_spin_lock(spin_lock_t* lock) TA_NO_THREAD_SAFETY_ANALYSIS {
+
+}
---
+++ kernel/arch/loongarch64/trap.cc
@@ -0,0 +1,2 @@
+extern "C" void arch_iframe_process_pending_signals(iframe_t* iframe) {
+}
