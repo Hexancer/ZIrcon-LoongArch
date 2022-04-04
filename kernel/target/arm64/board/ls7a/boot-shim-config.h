17a1aa4a93ef120bf220e69a76bfd4ae1ce6d09e
ghost <10137+ghost@users.noreply.github.com>
2022-04-05 00:37:53 +0800

    feat(bl,la): update board config
    Co-authored-by: gengyiyang <100781565@users.noreply.github.com>

--- kernel/target/arm64/board/ls7a/boot-shim-config.h
+++ kernel/target/arm64/board/ls7a/boot-shim-config.h
@@ -13,100 +13,93 @@ static size_t cpu_count = 0;
 static const zbi_mem_range_t mem_config[] = {
     {
         .type = ZBI_MEM_RANGE_RAM,
-        .paddr = 0x40000000,
-        .length = 0x08000000, // assume 512MB, FDT will provide the real number
+        .paddr = 0x90000000,
+        .length = 0x40000000,
     },
     {
         .type = ZBI_MEM_RANGE_PERIPHERAL,
         .paddr = 0,
-        .length = 0x40000000,
+        .length = 0x100000000, // DMA range, can we use it?
     },
 };
 
 static const dcfg_simple_t uart_driver = {
-    .mmio_phys = 0x09000000,
-    .irq = 33,
+    .mmio_phys = 0x1fe001e0,
+    .irq = 0, // TODO: irq number
 };
 
-static const dcfg_arm_gicv3_driver_t gicv3_driver = {
-    .mmio_phys = 0x08000000,
-    .gicd_offset = 0x00000,
-    .gicr_offset = 0xa0000,
-    .gicr_stride = 0x20000,
-    .ipi_base = 12,
-    .optional = true,
-};
+// static const dcfg_arm_gicv3_driver_t gicv3_driver = {
+//     .mmio_phys = 0x08000000,
+//     .gicd_offset = 0x00000,
+//     .gicr_offset = 0xa0000,
+//     .gicr_stride = 0x20000,
+//     .ipi_base = 12,
+//     .optional = true,
+// };
 
-static const dcfg_arm_gicv2_driver_t gicv2_driver = {
-    .mmio_phys = 0x08000000,
-    .msi_frame_phys = 0x08020000,
-    .gicd_offset = 0x00000,
-    .gicc_offset = 0x10000,
-    .ipi_base = 12,
-    .optional = true,
-    .use_msi = true,
-};
+// static const dcfg_arm_gicv2_driver_t gicv2_driver = {
+//     .mmio_phys = 0x08000000,
+//     .msi_frame_phys = 0x08020000,
+//     .gicd_offset = 0x00000,
+//     .gicc_offset = 0x10000,
+//     .ipi_base = 12,
+//     .optional = true,
+//     .use_msi = true,
+// };
 
-static const dcfg_arm_psci_driver_t psci_driver = {
-    .use_hvc = true,
-};
+// static const dcfg_arm_psci_driver_t psci_driver = {
+//     .use_hvc = true,
+// };
 
-static const dcfg_arm_generic_timer_driver_t timer_driver = {
-    .irq_phys = 30,
-    .irq_virt = 27,
-};
+// static const dcfg_arm_generic_timer_driver_t timer_driver = {
+//     .irq_phys = 30,
+//     .irq_virt = 27,
+// };
 
 static const zbi_platform_id_t platform_id = {
     .vid = PDEV_VID_QEMU,
     .pid = PDEV_PID_QEMU,
-    .board_name = "qemu",
+    .board_name = "ls7a",
 };
 
 static void add_cpu_topology(zbi_header_t* zbi) {
-  __builtin_trap();
-//    zbi_topology_node_t nodes[MAX_CPU_COUNT];
-//
-//    // clamp to the max cpu
-//    if (cpu_count > MAX_CPU_COUNT) {
-//        cpu_count = MAX_CPU_COUNT;
-//    }
-//
-//    for (size_t index = 0; index < cpu_count; index++) {
-//        nodes[index] = (zbi_topology_node_t){
-//            .entity_type = ZBI_TOPOLOGY_ENTITY_PROCESSOR,
-//            .parent_index = ZBI_TOPOLOGY_NO_PARENT,
-//            .entity = {
-//                .processor = {
-//                    .logical_ids = {index},
-//                    .logical_id_count = 1,
-//                    .flags = (index == 0) ? ZBI_TOPOLOGY_PROCESSOR_PRIMARY : 0,
-//                    .architecture = ZBI_TOPOLOGY_ARCH_ARM,
-//                    .architecture_info = {
-//                        .arm = {
-//                            // qemu seems to put 16 cores per aff0 level, max 32 cores.
-//                            .cluster_1_id = (index / 16),
-//                            .cpu_id = (index % 16),
-//                            .gic_id = index,
-//                        }}}}};
-//    }
-//
-//    append_boot_item(zbi, ZBI_TYPE_CPU_TOPOLOGY,
-//                     sizeof(zbi_topology_node_t), // Extra
-//                     &nodes, sizeof(zbi_topology_node_t) * cpu_count);
+   zbi_topology_node_t nodes[MAX_CPU_COUNT];
+
+   // clamp to the max cpu
+   if (cpu_count > MAX_CPU_COUNT) {
+       cpu_count = MAX_CPU_COUNT;
+   }
+
+   for (size_t index = 0; index < cpu_count; index++) {
+       nodes[index] = (zbi_topology_node_t){
+           .entity_type = ZBI_TOPOLOGY_ENTITY_PROCESSOR,
+           .parent_index = ZBI_TOPOLOGY_NO_PARENT,
+           .entity = {
+               .processor = {
+                   .logical_ids = {index},
+                   .logical_id_count = 1,
+                   .flags = (index == 0) ? ZBI_TOPOLOGY_PROCESSOR_PRIMARY : 0,
+                   .architecture = ZBI_TOPOLOGY_ARCH_LOONGARCH,
+                   .architecture_info = {
+                       .loongarch = {}}}}};
+   }
+
+   append_boot_item(zbi, ZBI_TYPE_CPU_TOPOLOGY,
+                    sizeof(zbi_topology_node_t), // Extra
+                    &nodes, sizeof(zbi_topology_node_t) * cpu_count);
 }
 
 static void append_board_boot_item(zbi_header_t* bootdata) {
-  __builtin_trap();
-//    add_cpu_topology(bootdata);
-//
-//    // add memory configuration
-//    append_boot_item(bootdata, ZBI_TYPE_MEM_CONFIG, 0, &mem_config,
-//                     sizeof(zbi_mem_range_t) * countof(mem_config));
-//
-//    // add kernel drivers
-//    append_boot_item(bootdata, ZBI_TYPE_KERNEL_DRIVER, KDRV_PL011_UART, &uart_driver,
-//                     sizeof(uart_driver));
-//
+   add_cpu_topology(bootdata);
+
+   // add memory configuration
+   append_boot_item(bootdata, ZBI_TYPE_MEM_CONFIG, 0, &mem_config,
+                    sizeof(zbi_mem_range_t) * countof(mem_config));
+
+   // add kernel drivers
+   append_boot_item(bootdata, ZBI_TYPE_KERNEL_DRIVER, KDRV_DW8250_UART, &uart_driver,
+                    sizeof(uart_driver));
+
 //    // append the gic information from the specific gic version we detected from the
 //    // device tree.
 //    if (saved_gic_version == 2) {
@@ -118,14 +111,14 @@ static void append_board_boot_item(zbi_header_t* bootdata) {
 //    } else {
 //        fail("failed to detect gic version from device tree\n");
 //    }
-//
+
 //    append_boot_item(bootdata, ZBI_TYPE_KERNEL_DRIVER, KDRV_ARM_PSCI, &psci_driver,
 //                     sizeof(psci_driver));
 //    append_boot_item(bootdata, ZBI_TYPE_KERNEL_DRIVER, KDRV_ARM_GENERIC_TIMER, &timer_driver,
 //                     sizeof(timer_driver));
-//
-//    // add platform ID
-//    append_boot_item(bootdata, ZBI_TYPE_PLATFORM_ID, 0, &platform_id, sizeof(platform_id));
+
+   // add platform ID
+   append_boot_item(bootdata, ZBI_TYPE_PLATFORM_ID, 0, &platform_id, sizeof(platform_id));
 }
 
 static void set_cpu_count(uint32_t new_count) {
