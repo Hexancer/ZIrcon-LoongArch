8fffe0baefe326d7cd19cda6e8d6fff34912a91d
ghost <10137+ghost@users.noreply.github.com>
2022-04-06 23:07:24 +0800

    feat(kernel,la): add simple spinlock
    Co-authored-by: gengyiyang <100781565@users.noreply.github.com>

--- kernel/arch/loongarch64/spinlock.cc
+++ kernel/arch/loongarch64/spinlock.cc
@@ -4,7 +4,20 @@
 #include <kernel/atomic.h>
 
 void arch_spin_lock(spin_lock_t* lock) TA_NO_THREAD_SAFETY_ANALYSIS {
-    TODO();
+    unsigned long val = arch_curr_cpu_num() + 1;
+    uint64_t temp;
+
+    __asm__ volatile(
+        "1:"                         // spin loop here
+        "ll.d  %[temp], %[lock], 0;" // load link from %lock
+        "bnez  %[temp], 1b;"         // %lock != 0 <==> some core holding it
+        "move  %[temp], %[val];"     // prepare val to write
+        "sc.d  %[temp], %[lock], 0;" // store cond to %lock
+        "beqz  %[temp], 1b;"         // %temp == 0 <==> SC failed, need retry
+        : [temp] "=&r"(temp)
+        : [lock] "r"(&lock->value), [val] "r"(val)
+        : "cc", "memory");
+    WRITE_PERCPU_FIELD32(num_spinlocks, READ_PERCPU_FIELD32(num_spinlocks) + 1);
 }
 
 int arch_spin_trylock(spin_lock_t* lock) TA_NO_THREAD_SAFETY_ANALYSIS {
