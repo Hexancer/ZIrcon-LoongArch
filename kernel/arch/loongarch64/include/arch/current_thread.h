c60dde39c5891289eded940612ce9bcae9665d20
ghost <10137+ghost@users.noreply.github.com>
2022-04-06 21:58:51 +0800

    feat(kernel,la): add percpu struct & $tp r/w
    Co-authored-by: gengyiyang <100781565@users.noreply.github.com>

--- kernel/arch/loongarch64/include/arch/current_thread.h
+++ kernel/arch/loongarch64/include/arch/current_thread.h
@@ -7,9 +7,12 @@ __BEGIN_CDECLS
 static inline struct thread* get_current_thread(void) {
     // TODO: do we need stack_guard like arm64
     void* tp;
-    __asm__ __volatile__("move $tp, %0"
-                         : "=r"(tp));
+    __asm__ __volatile__("move %0, $tp" : "=r"(tp));
     return (struct thread*)tp;
+}
+
+static inline void set_current_thread(struct thread* t) {
+    __asm__ __volatile__("move $tp, %0" : : "r"(t));
 }
 
 __END_CDECLS
--- kernel/arch/loongarch64/include/arch/loongarch64/mp.h
+++ kernel/arch/loongarch64/include/arch/loongarch64/mp.h
@@ -2,6 +2,7 @@
 
 #include <arch.h>
 #include <arch/loongarch64.h>
+#include <kernel/align.h>
 #include <kernel/cpu.h>
 
 
@@ -10,6 +11,31 @@ __BEGIN_CDECLS
 
 #define READ_PERCPU_FIELD32(field) (__builtin_trap(),0)
 #define WRITE_PERCPU_FIELD32(field, value) __builtin_trap()
+
+struct loongarch64_percpu {
+    // cpu number
+    uint32_t cpu_num;
+
+    // Whether blocking is disallowed.  See arch_blocking_disallowed().
+    uint32_t blocking_disallowed;
+
+    // Number of spinlocks currently held.
+    uint32_t num_spinlocks;
+} __CPU_ALIGN;
+
+
+// Use the x21 register to always point at the local cpu structure to allow fast access
+// a per cpu structure.
+// Do not directly access fields of this structure
+register struct loongarch64_percpu* __loongarch64_percpu __asm("r21");
+
+static inline void loongarch64_write_percpu_ptr(struct loongarch64_percpu* percpu) {
+    __loongarch64_percpu = percpu;
+}
+
+static inline struct loongarch64_percpu* loongarch64_read_percpu_ptr(void) {
+    return __loongarch64_percpu;
+}
 
 static inline cpu_num_t arch_curr_cpu_num(void) {
     return 0;
